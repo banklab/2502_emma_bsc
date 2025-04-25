825fa8f6e3f7ec67a0d1542fd092cbb5286017fd
emmaochsner <emma.ochsner@students.unibe.ch>
2025-04-25 12:09:10 +0200

    model 2 and cluster
    I changed the mateChoice command in model2. I also submitted some jobs to the cluster, thus generated cluster compatible slim files and .sh files.

--- cluster_models/cluster_model1_nrnr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --mem=20000M
-#SBATCH --time=1:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --array=1-1000
-
-/storage/scratch/users/eo20t062/SLiM/build/slim -d repetition=$SLURM_ARRAY_TASK_ID /storage/scratch/users/eo20t062/model1/slim_files/cluster_model1_nrnr.slim
---
+++ cluster_models/matings/cluster_matings_r_16.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --mem=20000M
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=<emma.ochsner@students.unibe.ch>
+#SBATCH --mail-type=FAIL,END
+
+for i in {1..1000}
+do
+	
+/storage/scratch/users/eo20t062/SLiM/build/slim -d repetition=$i /storage/scratch/users/eo20t062/matings/files/cluster_matings_r_16.slim
+
+done
