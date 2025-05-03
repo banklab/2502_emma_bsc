2bb78e61d2ebc8cba8b895a0dbe8738dd8f18710
emmaochsner <emma.ochsner@students.unibe.ch>
2025-05-03 17:16:36 +0200

    rel_het model on cluster
    I let the relatedness_heterozygosity model run on the cluster

---
+++ cluster_models/Rel_Het_model1_1000ind_100gen/cluster_r_rel_het_1000_model1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --mem=20000M
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=<emma.ochsner@students.unibe.ch>
+#SBATCH --mail-type=FAIL,END
+
+for i in {1..1000}
+do
+	
+/storage/scratch/users/eo20t062/SLiM/build/slim -d repetition=$i /storage/scratch/users/eo20t062/rel_het_model1/file_1000/cluster_r_rel_het_1000_model1.slim
+
+done
