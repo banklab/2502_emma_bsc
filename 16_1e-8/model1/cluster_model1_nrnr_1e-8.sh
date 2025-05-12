8ff8c9f411e03dbdc9c5f689442ee7bbf006e530
emmaochsner <emma.ochsner@students.unibe.ch>
2025-05-12 14:45:27 +0200

    update
    did some changes, as well as some additional recombination rates.

---
+++ 16_1e-8/model1/cluster_model1_nrnr_1e-8.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --mem=20000M
+#SBATCH --time=30:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+for i in {1..1000}
+do
+	
+/storage/scratch/users/eo20t062/SLiM/build/slim -d repetition=$i /storage/scratch/users/eo20t062/1e-8/model1/cluster_model1_nrnr_1e-8.slim
+
+done
---
+++ 16_1e-9/bottleneck/Bottleneck_start_1e-9_16.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --mem=20000M
+#SBATCH --time=6:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+/storage/scratch/users/eo20t062/SLiM/build/slim \
+    /storage/scratch/users/eo20t062/1e-9/bottleneck/Bottleneck_start_1e-9_16.slim
